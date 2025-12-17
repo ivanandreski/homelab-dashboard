aa812636b1e47d75b066dd3d078592cfb837274f
Ivan Andreski <>
2025-12-17 13:45:42 +0100

    init

---
+++ src/lib/features/hardware-monitor/HardwareMonitorWidget.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let cpuPercentage = $state<number>(35);
+  let memoryUsage = $state<number>(4.3);
+  const memoryMax = 16;
+
+  $effect(() => {
+    const interval = setInterval(() => {
+      cpuPercentage = (Math.floor(Math.random() * 100));
+      memoryUsage = parseFloat((Math.random() * memoryMax).toFixed(1));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+</script>
+
+<div>
+  <h2>Hardware Monitor</h2>
+  <div>
+    <strong>CPU Usage:</strong> {cpuPercentage}%
+  </div>
+  <div>
+    <strong>Memory Usage:</strong> {memoryUsage} GB / {memoryMax} GB
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import HardwareMonitorWidget from '$lib/features/hardware-monitor/HardwareMonitorWidget.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<HardwareMonitorWidget />
